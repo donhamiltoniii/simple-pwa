8e49731df0f2e42bedc9ecc6a51cd2cc7aae99e1
Don Hamilton III <donhamiltoniii@gmail.com>
2018-08-08 13:51:24 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+loadServiceWorker()
+
+function loadServiceWorker() {
+	if ('serviceWorker in navigator') {
+		navigator.serviceWorker
+			.register('./service-worker.js')
+			.then(() => console.log('Service Worker Registered'))
+	}
+}
